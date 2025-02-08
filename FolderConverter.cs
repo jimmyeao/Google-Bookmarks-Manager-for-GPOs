c7c58278f476ecc699db37c7e567c53fdac42e9d
Jimmy White <jimmyeao@users.noreply.github.com>
2025-02-08 00:05:10 +0000

    progress, tree view and text boxes work

---
+++ FolderConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Google_Bookmarks_Manager_for_GPOs  // Ensure this matches your project namespace
+{
+    public class FolderIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isFolder)
+            {
+                return isFolder ? "📁" : "🔖"; // Folder and Bookmark Emoji
+            }
+            return null;
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,115 +1,278 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace Google_Bookmarks_Manager_for_GPOs
 {
-    public class Bookmark
+    public class Bookmark : INotifyPropertyChanged
     {
-        #region Public Properties
+        private string _name;
+        private string _url;
+        private bool _isFolder;
+        private ObservableCollection<Bookmark> _children;
 
-        public string Name { get; set; }
-        public string Url { get; set; }
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
 
-        #endregion Public Properties
+        public string Url
+        {
+            get => _url;
+            set
+            {
+                _url = value;
+                OnPropertyChanged(nameof(Url));
+            }
+        }
+
+        public bool IsFolder
+        {
+            get => _isFolder;
+            set
+            {
+                _isFolder = value;
+                OnPropertyChanged(nameof(IsFolder));
+            }
+        }
+
+        public ObservableCollection<Bookmark> Children
+        {
+            get => _children;
+            set
+            {
+                _children = value;
+                OnPropertyChanged(nameof(Children));
+            }
+        }
+
+        public Bookmark()
+        {
+            Children = new ObservableCollection<Bookmark>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isFolder)
+            {
+                // If "IsFolder" is TRUE, hide the TextBox
+                return isFolder ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         #region Private Fields
 
-        private List<Bookmark> bookmarks;
-        private string topLevelName;
+        private ObservableCollection<Bookmark> _bookmarks;
+        private Bookmark _draggedBookmark;
+        public ObservableCollection<Bookmark> Bookmarks
+        {
+            get => _bookmarks;
+            set
+            {
+                if (_bookmarks != value)
+                {
+                    _bookmarks = value;
+                    OnPropertyChanged(nameof(Bookmarks));
+                }
+            }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         #endregion Private Fields
 
-        #region Public Constructors
-
-        private static bool _isDarkModeEnabled;
-
         public MainWindow()
         {
             InitializeComponent();
 
-            // Set up the data context
-            bookmarks = new List<Bookmark>();
-            bookmarks.Add(new Bookmark());
-            bookmarksDataGrid.ItemsSource = bookmarks;
-            bookmarksDataGrid.CanUserAddRows = true;
+            // Ensure initialization happens only once
+            if (Bookmarks == null)
+            {
+                Bookmarks = new ObservableCollection<Bookmark>();
+            }
+            SwitchTheme(true);
             DataContext = this;
-            // Clear the text boxes
-            bookmarkUrlTextBox.Clear();
-            bookmarkFolderNameTextBox.Clear();
-
-            // Clear the bookmarks list and refresh the data grid
-            bookmarks.Clear();
-            bookmarksDataGrid.Items.Refresh();
-
-            // Add an event handler for the PropertyChanged event
-            PropertyChanged += MainWindow_PropertyChanged;
         }
 
-        //private void SwitchTheme(bool isDark)
-        //{
-        //    var themeSource = isDark
-        //        ? "pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Dark.xaml"
-        //        : "pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Light.xaml";
+        #region Private Methods
 
-        //    var themeResource = Resources.MergedDictionaries
-        //        .Where(d => d.Source != null && (d.Source.OriginalString.Contains("MaterialDesignTheme.Light.xaml") || d.Source.OriginalString.Contains("MaterialDesignTheme.Dark.xaml")))
-        //        .FirstOrDefault();
-
-        //    if (themeResource != null)
-        //    {
-        //        Resources.MergedDictionaries.Remove(themeResource);
-        //    }
-
-        public static bool IsDarkModeEnabled
+        private void clearFormButton_Click(object sender, RoutedEventArgs e)
         {
-            get => _isDarkModeEnabled;
-            set
+         
+            bookmarkFolderNameTextBox.Clear();
+            Bookmarks.Clear();
+        }
+        private void TreeView_PreviewMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var treeView = sender as TreeView;
+            var item = treeView.SelectedItem as Bookmark;
+            if (item != null)
             {
-                if (_isDarkModeEnabled != value)
-                {
-                    _isDarkModeEnabled = value;
-                    UpdateTheme();
-                }
+                _draggedBookmark = item;
+                DragDrop.DoDragDrop(treeView, item, DragDropEffects.Move);
+            }
+        }
+        private void AddFolder_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is MenuItem menuItem && menuItem.CommandParameter is Bookmark parent)
+            {
+                parent.Children.Add(new Bookmark { Name = "New Folder", IsFolder = true });
             }
         }
 
-        private static void UpdateTheme()
+        private void AddBookmark_Click(object sender, RoutedEventArgs e)
         {
-            if (IsDarkModeEnabled)
+            if (sender is MenuItem menuItem && menuItem.CommandParameter is Bookmark parent)
             {
-                Application.Current.Resources.MergedDictionaries.Clear();
-                Application.Current.Resources.MergedDictionaries.Add(
-                    new ResourceDictionary { Source = new Uri("DarkTheme.xaml", UriKind.Relative) });
+                parent.Children.Add(new Bookmark { Name = "New Bookmark", Url = "http://", IsFolder = false });
+            }
+        }
+
+        private void DeleteBookmark_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is MenuItem menuItem && menuItem.CommandParameter is Bookmark bookmark)
+            {
+                RemoveBookmark(Bookmarks, bookmark);
+            }
+        }
+
+
+
+        private bool GetSelectedBookmark(object sender, out Bookmark selectedBookmark)
+        {
+            if (sender is MenuItem menuItem && menuItem.DataContext is Bookmark bookmark)
+            {
+                selectedBookmark = bookmark;
+                return true;
+            }
+            selectedBookmark = null;
+            return false;
+        }
+
+        private void TreeView_Drop(object sender, DragEventArgs e)
+        {
+            if (_draggedBookmark == null) return;
+
+            var target = (e.OriginalSource as FrameworkElement)?.DataContext as Bookmark;
+            if (target == null || target == _draggedBookmark) return;
+
+            // Remove from old location
+            RemoveBookmark(Bookmarks, _draggedBookmark);
+
+            // Only allow folders to receive children
+            if (!target.IsFolder) return;
+
+            // Add to new location
+            target.Children.Add(_draggedBookmark);
+
+            _draggedBookmark = null;
+        }
+
+
+        private bool RemoveBookmark(ObservableCollection<Bookmark> bookmarks, Bookmark bookmark)
+        {
+            foreach (var item in bookmarks)
+            {
+                if (item.Children.Contains(bookmark))
+                {
+                    item.Children.Remove(bookmark);
+                    return true;
+                }
+                if (RemoveBookmark(item.Children, bookmark)) return true;
+            }
+            return false;
+        }
+        private void exportBookmarksButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            var jsonObject = new JObject
+            {
+                ["roots"] = new JObject
+                {
+                    ["bookmark_bar"] = new JObject
+                    {
+                        ["name"] = "Bookmarks Bar",
+                        ["type"] = "folder",
+                        ["children"] = new JArray(Bookmarks.Select(ConvertBookmarkToJson))
+                    }
+                }
+            };
+
+            var json = jsonObject.ToString(Formatting.Indented);
+
+            var saveFileDialog = new Microsoft.Win32.SaveFileDialog
+            {
+                Filter = "Bookmark Files (*.json)|*.json"
+            };
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, json);
+            }
+        }
+
+        private JObject ConvertBookmarkToJson(Bookmark bookmark)
+        {
+            var obj = new JObject
+            {
+                ["name"] = bookmark.Name,
+                ["type"] = bookmark.IsFolder ? "folder" : "url"
+            };
+
+            if (!bookmark.IsFolder)
+            {
+                obj["url"] = bookmark.Url;
             }
             else
             {
-                Application.Current.Resources.MergedDictionaries.Clear();
-                Application.Current.Resources.MergedDictionaries.Add(
-                    new ResourceDictionary { Source = new Uri("LightTheme.xaml", UriKind.Relative) });
+                obj["children"] = new JArray(bookmark.Children.Select(ConvertBookmarkToJson));
             }
-        }
 
-        private void MainWindow_PropertyChanged(object sender, PropertyChangedEventArgs e)
+            return obj;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (e.PropertyName == nameof(IsDarkModeEnabled))
-            {
-                UpdateTheme();
-            }
+            var json = JsonConvert.SerializeObject(Bookmarks, Formatting.Indented);
+            Clipboard.SetText(json);
+            MessageBox.Show("Bookmarks copied to clipboard!", "Confirmation", MessageBoxButton.OK);
+        }
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            SwitchTheme(true);
         }
 
-        //    Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri(themeSource) });
-        //}
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            SwitchTheme(false);
+        }
         private void SwitchTheme(bool isDark)
         {
             var themeSource = isDark
@@ -117,8 +280,9 @@ namespace Google_Bookmarks_Manager_for_GPOs
                 : "pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Light.xaml";
 
             var themeResource = Application.Current.Resources.MergedDictionaries
-                .Where(d => d.Source != null && (d.Source.OriginalString.Contains("MaterialDesignTheme.Light.xaml") || d.Source.OriginalString.Contains("MaterialDesignTheme.Dark.xaml")))
-                .FirstOrDefault();
+                .FirstOrDefault(d => d.Source != null &&
+                    (d.Source.OriginalString.Contains("MaterialDesignTheme.Light.xaml") ||
+                     d.Source.OriginalString.Contains("MaterialDesignTheme.Dark.xaml")));
 
             if (themeResource != null)
             {
@@ -126,62 +290,6 @@ namespace Google_Bookmarks_Manager_for_GPOs
             }
 
             Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri(themeSource) });
-        }
-
-        #endregion Public Constructors
-
-        #region Private Methods
-
-        private void addBookmarkButton_Click(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private void bookmarksDataGrid_Loaded(object sender, RoutedEventArgs e)
-        {
-            if (bookmarks.Any())
-            {
-                bookmarks.RemoveAt(0);
-                bookmarksDataGrid.Items.Refresh();
-            }
-        }
-
-        private void clearFormButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Clear the text boxes
-            bookmarkUrlTextBox.Clear();
-            bookmarkFolderNameTextBox.Clear();
-
-            // Clear the bookmarks list and refresh the data grid
-            bookmarks.Clear();
-            bookmarksDataGrid.Items.Refresh();
-        }
-
-        private void exportBookmarksButton_Click_1(object sender, RoutedEventArgs e)
-        {
-            // Create a new JSON object with an array of bookmarks
-            JArray bookmarksArray = new JArray();
-            string topLevelName = bookmarkFolderNameTextBox.Text;
-            bookmarksArray.Add(new JObject(new JProperty("toplevel_name", topLevelName)));
-            foreach (Bookmark bookmark in bookmarks)
-            {
-                if (bookmark.Name != null || bookmark.Url != null) // check if the bookmark is not null
-
-                {
-                    JObject bookmarkObject = new JObject(new JProperty("name", bookmark.Name ?? ""), new JProperty("url", bookmark.Url ?? ""));
-                    bookmarksArray.Add(bookmarkObject);
-                }
-            }
-
-            // Convert the JSON object to a single line of text
-            var json = bookmarksArray.ToString(Formatting.None);
-
-            // Save the JSON string to a file
-            var saveFileDialog = new Microsoft.Win32.SaveFileDialog();
-            saveFileDialog.Filter = "Bookmark Files (*.json)|*.json";
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                File.WriteAllText(saveFileDialog.FileName, json);
-            }
         }
 
         private void importBookmarksButton_Click(object sender, RoutedEventArgs e)
@@ -199,15 +307,41 @@ namespace Google_Bookmarks_Manager_for_GPOs
             {
                 var parsedJson = JArray.Parse(json);
 
-                topLevelName = parsedJson.First["toplevel_name"].ToString();
-                bookmarkFolderNameTextBox.Text = topLevelName;
+                // Clear existing bookmarks
+                Bookmarks.Clear();
 
-                bookmarks = JsonConvert.DeserializeObject<List<Bookmark>>(parsedJson.ToString());
+                foreach (var item in parsedJson)
+                {
+                    // If the item contains a top-level name, treat it as a folder
+                    if (item["toplevel_name"] != null)
+                    {
+                        var topLevelFolder = new Bookmark
+                        {
+                            Name = item["toplevel_name"].ToString(),
+                            IsFolder = true
+                        };
 
-                // Remove the first element from the bookmarks list
-                bookmarks.RemoveAt(0);
+                        if (item["children"] != null)
+                        {
+                            foreach (var child in item["children"])
+                            {
+                                var bookmark = ParseBookmark(child);
+                                topLevelFolder.Children.Add(bookmark);
+                            }
+                        }
 
-                bookmarksDataGrid.ItemsSource = bookmarks;
+                        Bookmarks.Add(topLevelFolder);
+                    }
+                    else
+                    {
+                        // Directly parse other bookmarks that do not have a "toplevel_name"
+                        var bookmark = ParseBookmark(item);
+                        Bookmarks.Add(bookmark);
+                    }
+                }
+
+                // Force UI refresh
+                OnPropertyChanged(nameof(Bookmarks));
             }
             catch (Exception ex)
             {
@@ -215,55 +349,37 @@ namespace Google_Bookmarks_Manager_for_GPOs
             }
         }
 
-        #endregion Private Methods
 
-        #region Public Methods
-
-        public static MessageBoxResult ShowCustomMessageBox(string message, string caption, MessageBoxButton buttons)
+        private Bookmark ParseBookmark(JToken token)
         {
-            var customMessageBox = new CustomMessageBox(message, caption, buttons);
-            customMessageBox.Owner = Application.Current.MainWindow;
-            customMessageBox.WindowStartupLocation = WindowStartupLocation.CenterOwner;
-            customMessageBox.ShowDialog();
-            return customMessageBox.Result;
-        }
-
-        #endregion Public Methods
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            JArray bookmarksArray = new JArray();
-            string topLevelName = bookmarkFolderNameTextBox.Text;
-            bookmarksArray.Add(new JObject(new JProperty("toplevel_name", topLevelName)));
-            foreach (Bookmark bookmark in bookmarks)
+            var bookmark = new Bookmark
             {
-                if (bookmark.Name != null || bookmark.Url != null) // check if the bookmark is not null
+                Name = token["name"]?.ToString() ?? "Unnamed Bookmark",
+                IsFolder = token["children"] != null,
+                Url = token["url"]?.ToString() ?? ""  // Ensure URL is set even if missing
+            };
+
+            if (bookmark.IsFolder)
+            {
+                foreach (var child in token["children"])
                 {
-                    JObject bookmarkObject = new JObject(new JProperty("name", bookmark.Name ?? ""), new JProperty("url", bookmark.Url ?? ""));
-                    bookmarksArray.Add(bookmarkObject);
+                    bookmark.Children.Add(ParseBookmark(child));
                 }
             }
 
-            // Convert the JSON object to a single line of text
-            var json = bookmarksArray.ToString(Formatting.None);
-
-            // Copy the JSON string to the clipboard
-            Clipboard.SetText(json);
-
-            // Show a confirmation message
-            ShowCustomMessageBox("Bookmarks copied to clipboard!", "Confirmation", MessageBoxButton.OK);
+            return bookmark;
         }
 
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-            var isDark = darkModeCheckBox.IsChecked.HasValue && darkModeCheckBox.IsChecked.Value;
-            SwitchTheme(isDark);
-        }
 
-        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+
+
+
+
+        #endregion Private Methods
+
+        protected void OnPropertyChanged(string propertyName)
         {
-            var isDark = darkModeCheckBox.IsChecked.HasValue && darkModeCheckBox.IsChecked.Value;
-            SwitchTheme(isDark);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
